e180f9d403a88f56532ff5d4002297b59e0cf3ac
eaesmat <ea.ahmadzai2020@gmail.com>
2022-10-27 17:41:02 +0430

    done with all errors

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+# rubocop:disable all
 
 ActiveRecord::Schema[7.0].define(version: 20_221_027_104_018) do
   # These are extensions that must be enabled in order to support this database
@@ -91,3 +92,4 @@ ActiveRecord::Schema[7.0].define(version: 20_221_027_104_018) do
   add_foreign_key 'likes', 'users'
   add_foreign_key 'posts', 'users'
 end
+# rubocop:enable all
