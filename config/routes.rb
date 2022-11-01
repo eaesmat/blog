5c469c6401eaa791fe25ab358915fd375b7b94c2
eaesmat <ea.ahmadzai2020@gmail.com>
2022-11-01 19:25:38 +0430

    Add controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+ root 'users#index'
+ get 'posts/index'
+ resources :users
+ resources :posts
 end
