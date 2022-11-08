9141f90a7141592e319d409ac1cbeaeb05e24635
eaesmat <ea.ahmadzai2020@gmail.com>
2022-11-08 14:30:55 +0430

    Add integration test

---
+++ spec/views/user_index_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Users Index Page', type: :feature do
+  before :each do
+    @user = User.create(name: 'Esmat', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                        bio: 'I am a Full-stack developer', posts_counter: 6)
+    @user2 = User.create(name: 'Ahmadzai', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                         bio: 'I am a software developer', posts_counter: 9)
+  end
+  scenario 'displays all users username' do
+    visit '/'
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content(@user2.name)
+  end
+
+  scenario 'displays all users profile photo' do
+    visit user_path(id: @user.id)
+    visit user_path(id: @user2.id)
+    find("img[src='https://unsplash.com/photos/F_-0BxGuVvo']")
+    find("img[src='https://unsplash.com/photos/F_-0BxGuVvo']")
+  end
+
+  scenario 'displays number of posts' do
+    visit '/'
+    expect(page).to have_content(@user.posts_counter)
+  end
+
+  scenario 'renders the user show page' do
+    visit '/'
+    click_link(@user.name)
+    expect(page).to have_content(@user.name)
+  end
+end
