1a0f6e7bb22804faaf9f6d3fe1e0ef5b78734987
ifzyy <johnsonnifemi8@gmail.com>
2022-11-09 11:14:08 +0100

    fixed N+1 issue

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,10 @@ class Post < ApplicationRecord
   belongs_to :user, class_name: 'User'
 
   # validates :title, presence: { strict: true }, length: { minimum: 3, maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_save :update_post_counter
+  after_save :update_posts_counter
 
   def update_posts_counter
     user.increment!(:post_counter)
