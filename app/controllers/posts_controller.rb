4bb28a348d8002a0e2a3ae8dc09da7fa55a8b76d
ifzyy <johnsonnifemi8@gmail.com>
2022-11-09 11:22:41 +0100

    fixed linter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
 
   def show
-   @post = Post.includes(comments: [:author]).find(params[:id])
+    @post = Post.includes(comments: [:author]).find(params[:id])
   end
 
   def new
